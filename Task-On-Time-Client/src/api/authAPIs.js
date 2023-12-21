f5d285dda8f7ba996217e646c01dba847a01e4c7
nhasan97 <nh6069325@gmail.com>
2023-12-21 20:41:24 +0600

    deployed client side on firebase

--- Task-On-Time-Client/src/api/authAPIs.js
+++ Task-On-Time-Client/src/api/authAPIs.js
@@ -4,10 +4,10 @@ export const saveUserData = async (user) => {
   const currentUser = {
     name: user.displayName,
     email: user.email,
-    role: "admin",
+    role: "member",
     groupId: "",
   };
-
+  console.log(currentUser);
   const { data } = await axiosSecure.put(`/users/${user?.email}`, currentUser);
 
   return data;
--- Task-On-Time-Client/src/providers/AuthProvider.jsx
+++ Task-On-Time-Client/src/providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axiosSecure from "../api/axiosSecure";
 
 export const AuthContext = createContext();
 
@@ -48,8 +49,21 @@ const AuthProvider = ({ children }) => {
   //==================== Getting current logged in user ====================
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email; //once set current user will become null thats why we are trying to get the users email before setting the user
+      const loggedUser = { email: userEmail };
       setUser(currentUser);
-      setLoading(false);
+
+      if (currentUser) {
+        axiosSecure
+          .post("/jwt", loggedUser)
+          .then((res) => console.log(res.data));
+        setLoading(false);
+      } else {
+        axiosSecure
+          .post("/logout", loggedUser)
+          .then((res) => console.log(res.data));
+        setLoading(false);
+      }
     });
 
     return () => {
