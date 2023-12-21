f6de35cca0b5626ce6674d9ec5398ef99ff7af6d
nhasan97 <nh6069325@gmail.com>
2023-12-21 12:33:46 +0600

    created authprovider for providing authcontext containing methods for login, register and logout

---
+++ Task-On-Time-Client/src/providers/AuthProvider.jsx
@@ -0,0 +1,85 @@
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+  const [loading, setLoading] = useState(true);
+
+  //==================== Register Using Email and Password ====================
+  const registerWithEmailAndPassword = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //==================== Update User's Profile ====================
+  const updateUsersProfile = (name, photo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  //==================== Login Using Email and Password ====================
+  const loginWithEmailAndPassword = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //================== Register/Login using Google ==================
+  const signInWithGoogle = (provider) => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  //==================== Getting current logged in user ====================
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  //==================== Logout User ====================
+  const logoutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const authInfo = {
+    auth,
+    user,
+    loading,
+    registerWithEmailAndPassword,
+    updateUsersProfile,
+    loginWithEmailAndPassword,
+    signInWithGoogle,
+    logoutUser,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default AuthProvider;
